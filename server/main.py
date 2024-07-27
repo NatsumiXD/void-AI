90a4b79975b7dc006662907c81c397194f5d0ab8
Roren <RorenNatsumi@outlook.com>
2024-07-27 14:07:37 +0800

    test

--- server/main.py
+++ server/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from datetime import datetime
-import hextt
+from . import hextt
 import json
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
